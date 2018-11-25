8ce791490006c4b3271253ccaad918cc8c80c8b9
Furkan Süğlün <suglunfurkan@gmail.com>
2018-11-26 02:30:57 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,77 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import FCM, {
+    FCMEvent,
+    WillPresentNotificationResult,
+    NotificationType,
+} from "react-native-fcm";
+
+export default class App extends Component<Props> {
+    async componentWillMount() {
+        // herhangi bir bildirim etkileşiminde çalışır
+        FCM.on(FCMEvent.Notification, notif => {
+            console.log("Bildirim geldi", notif);
+
+            if(Platform.OS ==='ios' && notif._notificationType === NotificationType.WillPresent && !notif.local_notification){
+                // Bu bildirim yalnızca kullanıcı ön planda ise bildirimi göstermek isteyip istemediğinize karar vermektir.
+                // genellikle görmezden gelebilirsiniz. sadece göster/gösterme yapar.
+                notif.finish(WillPresentNotificationResult.All)
+                return;
+            }
+        });
+
+        // eğer token yenilenirse çalışır
+        FCM.on(FCMEvent.RefreshToken, token => {
+            console.log("TOKEN YENİLENDİ (refreshUnsubscribe)", token);
+        });
+
+        // Uygulamaya Push atmak için izin alıyoruz
+        try {
+            let result = await FCM.requestPermissions({badge: false, sound: true, alert: true});
+        } catch(e){
+            console.error(e);
+        }
+
+        // Firebase Cloud Message Token değerini getirir.
+        FCM.getFCMToken().then(token => {
+            console.log("TOKEN (getFCMToken)", token);
+        });
+
+        if(Platform.OS === 'ios') {
+            // Eğer APNS istiyorsanız isteğe bağlı APNS TOKEN
+            FCM.getAPNSToken().then(token => {
+                console.log("APNS TOKEN (getAPNSToken)", token);
+            });
+        }
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>
+                    React Native Firebase Push Notification Kurulumu Uygulaması.
+                    Daha fazla bilgi için: <Text style={{color: 'blue'}}>github.com/abdurrahmanekr/rnpushapp</Text>
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# ReactNative_PushNotificationFirebase
+# ReactNative - PushNotification with Firebase
+
+react-native-firebase now can do what react-native-fcm can so it is a waste of effort to build the same thing in parallel.
+
+Since I'm getting busier these days and start feeling challenging to maintain this repo every day while react-native-firebase has a larger team/company backing it, existing users may consider migrating to react-native-firebase.
+
+I've created an example project using react-native-firebase as a migration reference
+
+react-native-fcm will still take PRs and bug fixes, but possibly no new feature developement any more.
+
+
+# Installation
+
+Run npm install react-native-fcm --save
+Link libraries Note: the auto link doesn't work with xcworkspace so CocoaPods user needs to do manual linking
+You many need this package for huawei phone
+
+# Configure Firebase Console
+
+In firebase console, you can:
+
+for Android: download google-services.json file and place it in android/app directory.
+for iOS: download GoogleService-Info.plist file and place it in /ios/your-project-name directory (next to your Info.plist)
+Make sure you have certificates setup by following https://firebase.google.com/docs/cloud-messaging/ios/certs
